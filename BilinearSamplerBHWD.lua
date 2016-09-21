8ca61c5be3178bc9f1495e54aaabe93c8a410a7c
Jianwei Yang <jw2yang@devgpu195.prn2.facebook.com>
2016-09-20 17:59:54 -0700

    add one more input canvas

--- BilinearSamplerBHWD.lua
+++ BilinearSamplerBHWD.lua
@@ -6,7 +6,7 @@ local BilinearSamplerBHWD, parent = torch.class('nn.BilinearSamplerBHWD', 'nn.Mo
    BilinearSamplerBHWD:updateGradInput({inputImages, grids}, gradOutput)
 
    BilinearSamplerBHWD will perform bilinear sampling of the input images according to the
-   normalized coordinates provided in the grid. Output will be of same size as the grids, 
+   normalized coordinates provided in the grid. Output will be of same size as the grids,
    with as many features as the input images.
 
    - inputImages has to be in BHWD layout
@@ -25,12 +25,16 @@ end
 
 function BilinearSamplerBHWD:check(input, gradOutput)
    local inputImages = input[1]
-	local grids = input[2]
+	 local grids = input[2]
+   local canvas = input[3]
 
    assert(inputImages:isContiguous(), 'Input images have to be contiguous')
    assert(inputImages:nDimension()==4)
+   assert(canvas:isContiguous(), 'Canvas images have to be contiguous')
+   assert(canvas:nDimension()==4)
    assert(grids:nDimension()==4)
    assert(inputImages:size(1)==grids:size(1)) -- batch
+   assert(inputImages:size(1)==canvas:size(1)) -- batch
    assert(grids:size(4)==2) -- coordinates
 
    if gradOutput then
@@ -50,66 +54,77 @@ local function addOuterDim(t)
    return t:view(newsizes)
 end
 
+-- Different from the original version, we have three inputs here:
+-- input[1]: input images
+-- input[2]: grid
+-- input[3]: canvas images
 function BilinearSamplerBHWD:updateOutput(input)
    local _inputImages = input[1]
    local _grids = input[2]
+   local _canvas = input[3]
 
-   local inputImages, grids
+   local inputImages, grids, canvas
    if _inputImages:nDimension()==3 then
       inputImages = addOuterDim(_inputImages)
       grids = addOuterDim(_grids)
    else
       inputImages = _inputImages
       grids = _grids
+      canvas = _canvas
    end
 
-   local input = {inputImages, grids}
+   local input = {inputImages, grids, canvas}
 
    self:check(input)
 
    self.output:resize(inputImages:size(1), grids:size(2), grids:size(3), inputImages:size(4))
+
+   -- Note: instead of set all value be zero, we copy the value from canvas
+   self.output:copy(canvas)
 
    inputImages.nn.BilinearSamplerBHWD_updateOutput(self, inputImages, grids, self.output)
 
    if _inputImages:nDimension()==3 then
       self.output=self.output:select(1,1)
    end
-	
+
    return self.output
 end
 
 function BilinearSamplerBHWD:updateGradInput(_input, _gradOutput)
-	local _inputImages = _input[1]
-	local _grids = _input[2]
+  local _inputImages = _input[1]
+  local _grids = _input[2]
+  local _canvas = _input[3]
 
-   local inputImages, grids, gradOutput
-   if _inputImages:nDimension()==3 then
-      inputImages = addOuterDim(_inputImages)
-      grids = addOuterDim(_grids)
-      gradOutput = addOuterDim(_gradOutput)
-   else
-      inputImages = _inputImages
-      grids = _grids
-      gradOutput = _gradOutput
-   end
+  local inputImages, grids, canvas, gradOutput
+  if _inputImages:nDimension()==3 then
+    inputImages = addOuterDim(_inputImages)
+    grids = addOuterDim(_grids)
+    gradOutput = addOuterDim(_gradOutput)
+  else
+    inputImages = _inputImages
+    grids = _grids
+    gradOutput = _gradOutput
+    canvas = _canvas
+  end
 
-   local input = {inputImages, grids}
+  local input = {inputImages, grids, canvas}
 
-   self:check(input, gradOutput)
+  self:check(input, gradOutput)
 	for i=1,#input do
-	   self.gradInput[i] = self.gradInput[i] or input[1].new()
-      self.gradInput[i]:resizeAs(input[i]):zero()
-   end
+    self.gradInput[i] = self.gradInput[i] or input[1].new()
+    self.gradInput[i]:resizeAs(input[i]):zero()
+  end
 
-   local gradInputImages = self.gradInput[1]
-   local gradGrids = self.gradInput[2]
+  local gradInputImages = self.gradInput[1]
+  local gradGrids = self.gradInput[2]
 
-   inputImages.nn.BilinearSamplerBHWD_updateGradInput(self, inputImages, grids, gradInputImages, gradGrids, gradOutput)
+  inputImages.nn.BilinearSamplerBHWD_updateGradInput(self, inputImages, grids, gradInputImages, gradGrids, gradOutput)
 
-   if _gradOutput:nDimension()==3 then
-      self.gradInput[1]=self.gradInput[1]:select(1,1)
-      self.gradInput[2]=self.gradInput[2]:select(1,1)
-   end
-   
-   return self.gradInput
+  if _gradOutput:nDimension()==3 then
+    self.gradInput[1]=self.gradInput[1]:select(1,1)
+    self.gradInput[2]=self.gradInput[2]:select(1,1)
+  end
+
+  return self.gradInput
 end
