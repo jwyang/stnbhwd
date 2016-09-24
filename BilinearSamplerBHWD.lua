a38a771b9c9f6ab2f18ecf39eeed3de058d57957
jw2.yang@gmail.com <jw2yang@devgpu195.prn2.facebook.com>
2016-09-23 17:10:48 -0700

    change

--- BilinearSamplerBHWD.lua
+++ BilinearSamplerBHWD.lua
@@ -78,7 +78,6 @@ function BilinearSamplerBHWD:updateOutput(input)
    end
 
    local input = {inputImages, grids, masks, canvas}
-   print(input)
    self:check(input)
 
    self.output:resize(inputImages:size(1), grids:size(2), grids:size(3), inputImages:size(4))
@@ -130,7 +129,7 @@ function BilinearSamplerBHWD:updateGradInput(_input, _gradOutput)
   local gradMasks = self.gradInput[3]
   local gradCanvas = self.gradInput[4]
 
-  inputImages.nn.BilinearSamplerBHWD_updateGradInput(self, inputImages, grids, masks, canvas, gradInputImages, gradGrids, gradMasks, gradCanvas, gradOutput)
+  -- inputImages.nn.BilinearSamplerBHWD_updateGradInput(self, inputImages, grids, masks, canvas, gradInputImages, gradGrids, gradMasks, gradCanvas, gradOutput)
 
   if _gradOutput:nDimension()==3 then
     self.gradInput[1]=self.gradInput[1]:select(1,1)
